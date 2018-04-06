bbe724e1186ba3c285203ede71bb9e0ea494128c
Soya Li <siyuli@cisco.com>
2018-04-06 23:32:48 +0800

    client compile

--- HomeSchoolClient/app/src/main/java/com/linuxgroup/homeschool/client/api/ApiInterface.java
+++ HomeSchoolClient/app/src/main/java/com/linuxgroup/homeschool/client/api/ApiInterface.java
@@ -4,7 +4,7 @@ package com.linuxgroup.homeschool.client.api;
  * Created by tan on 14-9-21.
  */
 public class ApiInterface {
-    public static final String BASE_URL = "http://192.168.1.172:8080";
+    public static final String BASE_URL = "http://localhost:8080";
     public static final String PATH_MESSAGE = BASE_URL + "/restful/message";
     public static final String PATH_GET_LATEST_VERSION = BASE_URL + "/restful/update/latestVersion";
     public static final String PATH_DOWNLOAD_PATH = BASE_URL + "/restful/update/downloadPath";
